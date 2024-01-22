bc42dceb5b69d04c1f7eb9741e429f314b3847c9
Madhava Rayulu <100406503+madhavarayulu@users.noreply.github.com>
2024-01-22 16:46:46 +0000

    complete 1.6: unicafe step1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,61 +1,14 @@
-const Header = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      {props.parts.map((part, index) => (
-        <p key={index}>{part.name} {part.exercises}</p>
-      ))}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  const partsArray = props.parts
-  let totalExercises = 0
-  {partsArray.forEach(part => {
-    totalExercises += part.exercises
-  })}
-  return (
-    <div>
-      <p>Number of exercises {totalExercises}</p>
-    </div>
-  )
-}
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      code here
     </div>
   )
 }
