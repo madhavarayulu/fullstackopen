81dab5509b40e42cff00acfa1c9a717afa1d4b6f
Madhava Rayulu <100406503+madhavarayulu@users.noreply.github.com>
2024-01-23 16:41:27 +0000

    complete 1.14*: anecdotes step3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,71 +1,5 @@
 import { useState } from "react";
 
-const Button = (props) => {
-  return (
-    <div>
-      <button onClick={props.handleClick}>{props.text}</button>
-    </div>
-  )
-}
-
-const StatisticLine = (props) => {
-  return (
-    <>
-      {props.text} {props.value}
-    </>
-  )
-}
-
-const Statistics = (props) => {
-  console.log(props)
-  const totalFeedbacks = props.good + props.neutral + props.bad
-  const averageFeedbacks = (props.good + props.neutral + props.bad)/3
-  const positivePercentage = (props.good * 100)/(props.good + props.neutral + props.bad) + "%"
-  
-  if (props.good === 0 && props.neutral === 0 && props.bad ===0) {
-    return (
-      <div>
-        <h1>statistics</h1>
-        No feedback given
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h1>statistics</h1>
-      <table>
-        <tbody>
-          <tr>
-            <td><StatisticLine text="good" /></td>
-            <td><StatisticLine value={props.good} /></td>
-          </tr>
-          <tr>
-            <td><StatisticLine text="neutral" /></td>
-            <td><StatisticLine value={props.neutral} /></td>
-          </tr>
-          <tr>
-            <td><StatisticLine text="bad"  /></td>
-            <td><StatisticLine value={props.bad} /></td>
-          </tr>
-          <tr>
-            <td><StatisticLine text="all" /></td>
-            <td><StatisticLine value={totalFeedbacks} /></td>
-          </tr>
-          <tr>
-            <td><StatisticLine text="average" /></td>
-            <td><StatisticLine value={averageFeedbacks} /></td>
-          </tr>
-          <tr>
-            <td><StatisticLine text="positive" /></td>
-            <td><StatisticLine value={positivePercentage} /></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  )
-}
-
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -80,13 +14,10 @@ const App = () => {
 
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
 
   const handleAnecdote = () => {
-    setSelected(Math.floor(Math.random()*(anecdotes.length)))
+    const randomAnecdoteNumber = Math.floor(Math.random()*(anecdotes.length))
+    setSelected(randomAnecdoteNumber)
   }
 
   const handleVote = () => {
@@ -97,24 +28,27 @@ const App = () => {
     })
   }
 
+  const maxVotes = Math.max(...votes)
+  let maxVoteIndex = 0
+  for (let i=0; i<votes.length; i++) {
+    if (votes[i] === maxVotes) {
+      maxVoteIndex = i
+    }
+  }
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
       <br />
       has {votes[selected]} votes
       <br />
       <button onClick={handleVote}>vote</button>
       <button onClick={handleAnecdote}>next anecdote</button>
-      {console.log(votes)}
-      {console.log(selected)}
-
-      <h1>give feedback</h1>
-      <div style={{ display: "flex" }}>
-        <Button handleClick={() => setGood(good + 1)} text="good" />
-        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-        <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      </div>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[maxVoteIndex]}
+      <br />
+      has {votes[maxVoteIndex]} votes
     </div>
   )
 }
