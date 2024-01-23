b98f8a3bd4bb9f72ec72810c963b68446f861c51
Madhava Rayulu <100406503+madhavarayulu@users.noreply.github.com>
2024-01-23 12:56:39 +0000

    complete 1.12*: anecdotes step1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -67,6 +67,18 @@ const Statistics = (props) => {
 }
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -74,6 +86,11 @@ const App = () => {
 
   return (
     <div>
+      {anecdotes[selected]}
+      <br />
+      {console.log(selected)}
+      <button onClick={() => setSelected(Math.floor(Math.random()*(anecdotes.length)))}>next anecdote</button>
+      {console.log(selected)}
       <h1>give feedback</h1>
       <div style={{ display: "flex" }}>
         <Button handleClick={() => setGood(good + 1)} text="good" />
