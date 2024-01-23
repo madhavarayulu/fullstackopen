75fe1beb65a57172d689b25b0b8bd83c15e770b5
Madhava Rayulu <100406503+madhavarayulu@users.noreply.github.com>
2024-01-23 06:29:13 +0000

    correct and completed 1.6: unicafe step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,7 +8,20 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <div>
+        <h1>give feedback</h1>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+      <div>
+        <h1>statistics</h1>
+        good {good}
+        <br />
+        neutral {neutral}
+        <br />
+        bad {bad}
+      </div>
     </div>
   )
 }
