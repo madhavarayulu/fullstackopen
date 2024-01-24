dd801247a202ac097b25339ad60a2816404308a9
Madhava Rayulu <100406503+madhavarayulu@users.noreply.github.com>
2024-01-24 09:05:11 +0000

    complete 2.3, 2.4, 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -6,9 +6,13 @@ import Total from "./Total.jsx"
 const Course = ({ course }) => {
     return (
       <div>
-        <Header course={course} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
+        <h1>Web development curriculum</h1>
+        <Header course={course[0]} />
+        <Content parts={course[0].parts} />
+        <Total parts={course[0].parts} />
+        <Header course={course[1]} />
+        <Content parts={course[1].parts} />
+        <Total parts={course[1].parts} />
       </div>
     )
 }
