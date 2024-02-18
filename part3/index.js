54be015881f63678c4399004182b8d7941ab4ff7
madhavarayulu <madhavarayulu@gmail.com>
2024-02-18 20:02:36 +0530

    complete 3.4: Phonebook backend step 4

--- part3/index.js
+++ part3/index.js
@@ -2,26 +2,26 @@ const express = require("express");
 const app = express();
 
 let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
   },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
   },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
   },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ];
 
 app.get("/api/persons", (request, response) => {
@@ -36,6 +36,12 @@ app.get("/api/persons/:id", (request, response) => {
   } else {
     response.sendStatus(404);
   }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.sendStatus(204);
 });
 
 app.get("/info", (request, response) => {
