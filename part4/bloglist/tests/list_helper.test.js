e980f3918ce3b095f5355d7c404e93a3987e7175
madhavarayulu <madhavarayulu@gmail.com>
2024-03-12 13:40:24 +0530

    complete 4.5*: Helper Functions and Unit Tests, step 3

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -67,3 +67,49 @@ describe("total likes", () => {
     assert.strictEqual(result, expected);
   });
 });
+
+describe("favorite blog", () => {
+  test("should return the blog with the most likes", () => {
+    const blogs = [
+      {
+        title: "React patterns",
+        author: "Michael Chan",
+        likes: 7,
+      },
+      {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        likes: 5,
+      },
+      {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12,
+      },
+      {
+        title: "First class tests",
+        author: "Robert C. Martin",
+        likes: 10,
+      },
+      {
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        likes: 0,
+      },
+      {
+        title: "Type wars",
+        author: "Robert C. Martin",
+        likes: 2,
+      },
+    ];
+
+    const expected = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    };
+
+    const result = listHelper.favoriteBlog(blogs);
+    assert.deepStrictEqual(result, expected)
+  });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  // Find the blog with the maximum likes using reduce
+  const mostLikedBlog = blogs.reduce((maxBlog, currentBlog) => {
+    return currentBlog.likes > maxBlog.likes ? currentBlog : maxBlog;
+  }, blogs[0]); // Initialize with the first blog
+
+  return {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
