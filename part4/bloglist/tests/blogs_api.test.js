17b484c5ea5d10c4c7cbc22f4e3133a0b9cb04c1
madhavarayulu <madhavarayulu@gmail.com>
2024-03-17 03:08:07 +0530

    complete 4.10: Blog List Tests, step 3

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -3,6 +3,7 @@ const assert = require('node:assert');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog');
 
 const api = supertest(app);
 
@@ -13,15 +14,35 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/);
 });
 
-test('there are two blogs', async () => {
-  const response = await api.get('/api/blogs');
-  assert.strictEqual(response.body.length, 2);
-});
+// test('there are two blogs', async () => {
+//   const response = await api.get('/api/blogs');
+//   assert.strictEqual(response.body.length, 2);
+// });
 
 test('the unique identifier property of the blog posts is named id', async () => {
   const response = await api.get('/api/blogs')
   const hasIdProperty = response.body.every(obj => Object.keys(obj).includes('id'))
   assert.strictEqual(hasIdProperty, true)
+})
+
+test('new blog post creationg successfull', async () => {
+  const initialBlogCount = await Blog.countDocuments()
+
+  const newBlog = {
+    title: `Capitalist Markets Aren't “Free.” They're Planned for Profit`,
+    author: 'Grace Blakeley'
+  }
+
+  const response = await api.post('/api/blogs')
+                            .send(newBlog)
+                            .expect(201)
+                            .expect('Content-Type', /application\/json/)
+  
+  const updatedBlogCount = await Blog.countDocuments()
+
+  assert.strictEqual(updatedBlogCount, initialBlogCount + 1)
+  assert.strictEqual(response.body.title, newBlog.title)
+  assert.strictEqual(response.body.author, newBlog.author)
 })
 
 after(async () => {
